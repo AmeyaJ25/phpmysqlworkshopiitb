40ac42a4c189e6552988f9031be949b1dca0d83d
AmeyaJ25 <72141126+AmeyaJ25@users.noreply.github.com>
2020-10-14 12:56:21 +0530

    First commit

---
+++ Day6&7/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Portal</title>
+    <style>
+    .container {
+    padding-top:30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+    img{
+        padding-left:650px;
+    }
+    button{
+        height: 40px;
+        width: 120px;
+        margin: 10px;
+    }
+    </style>
+</head>
+<body>
+    <h1 style="text-align: center;">Student Result Storage System</h1>
+    <img src="marks.png" alt="Marksheet">
+    <div class="container">
+    <a href="login.php"><button><b>Login</b></button></a>
+    <a href="register.php"><button><b>Register</b></button></a>
+    <a href="adminlogin.php"><button><b>Admin Login</b></button></a>
+    </div>
+</body>
+</html>
